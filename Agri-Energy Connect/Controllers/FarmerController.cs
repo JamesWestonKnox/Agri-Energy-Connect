aae871d974d4f95f1203d930631a6018cf998c54
James Knox <jamesknox361@gmail.com>
2025-05-13 11:27:00 +0200

    Added farmer product and addproduct pages

--- Agri-Energy Connect/Controllers/FarmerController.cs
+++ Agri-Energy Connect/Controllers/FarmerController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Agri_Energy_Connect.Data;
+using Agri_Energy_Connect.Models;
+using Agri_Energy_Connect.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Agri_Energy_Connect.Controllers
 {
     public class FarmerController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public FarmerController(AppDbContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public IActionResult FarmerDashboard()
@@ -11,6 +21,74 @@ namespace Agri_Energy_Connect.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Products()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+
+            var farmer = _context.Farmers
+                .Include(f => f.Products)
+                .FirstOrDefault(f => f.UserId == userId);
+
+            if (farmer == null)
+            {
+                return NotFound("Farmer not found for this user.");
+            }
+
+            return View(farmer.Products.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(AddProductModel model) 
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = HttpContext.Session.GetInt32("UserId");
+                if (userId == null)
+                {
+                    ModelState.AddModelError("", "Session expired. Please log in again.");
+                    return RedirectToAction("Login", "Account");
+                }
+
+                var farmer = _context.Farmers.FirstOrDefault(f => f.UserId == userId);
+                if (farmer == null)
+                {
+                    ModelState.AddModelError("", "Farmer not found.");
+                    return View(model);
+                }
+
+                var product = new Product
+                {
+                    Name = model.Name,
+                    Category = model.Category,
+                    Description = model.Description,
+                    ProductionDate = model.ProductionDate,
+                    Price = model.Price,
+                    FarmerId = farmer.FarmerId
+                };
+
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                TempData["Success"] = "Product added successfully.";
+                return RedirectToAction("Products");
+            }
+            else
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+
+                return View(model);
+            }
+        }
 
     }
 }
--- Agri-Energy Connect/Models/AddProductModel.cs
+++ Agri-Energy Connect/Models/AddProductModel.cs
@@ -1,15 +1,22 @@
-﻿namespace Agri_Energy_Connect.Models
-{
-    public class Product
-    {
-        public int ProductId { get; set; }
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace Agri_Energy_Connect.Models
+{
+    public class AddProductModel
+    {
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Category { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public DateTime ProductionDate { get; set; }
+
+        [Required]
         public double Price { get; set; }
-        public int FarmerId { get; set; }
-        public Farmer Farmer { get; set; }
     }
 }
